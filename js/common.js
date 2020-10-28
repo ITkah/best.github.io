cc76255aea8dc67f79561afb7b50c59c8ebfe18a
ITkah <42772919+ITkah@users.noreply.github.com>
2020-10-28 14:22:59 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$('.intresting_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+});
+
+$('.steps_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+});
